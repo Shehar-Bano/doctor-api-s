2f6fa5b02555e0b980ad10472b9d95a693d3e25f
Shehar-Bano <bshehar2002@gmail.com>
2024-08-28 13:31:01 +0500

    list add

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,13 @@ Route::get('/user', function (Request $request) {
 Route::group(['prefix'=>'auth'], function ($router) {
     Route::post('login', [AuthController::class,'login']);
     Route::post('signup', [AuthController::class,'register']);
+    Route::get('list',[AuthController::class,'index']);
 });
 Route::middleware(['auth:api'])->group(function(){
     Route::post('logout', [AuthController::class,'logout']);
     Route::post('refresh', [AuthController::class,'refresh']);
     Route::post('me', [AuthController::class,'me']);
-    Route::get('list',[AuthController::class,'index']);
+
 });
 //////////////////feature
 Route::get('/feature', [FeatureController::class,'index']);
